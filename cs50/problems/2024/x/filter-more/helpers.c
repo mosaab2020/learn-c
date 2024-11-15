558816acf67f74c52b2b4ddf81220a40891fb565
mosaab2020 <mosaabomar@gmail.com>
2024-11-15 16:06:05 +0400

    update

--- cs50/problems/2024/x/filter-more/helpers.c
+++ cs50/problems/2024/x/filter-more/helpers.c
@@ -1,14 +1,17 @@
-#include "helpers.h"
+﻿#include "helpers.h"
 #include <math.h>
-#include <stdbool.h>
 #include <stdio.h>
+
+RGBTRIPLE pixel(int r, int g, int b) {
+  RGBTRIPLE p;
+  p.rgbtRed = r;
+  p.rgbtGreen = g;
+  p.rgbtBlue = b;
+  return p;
+}
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width]) {
-  /* TODO:
-   * calulate the average pixel value -- done
-   * set each color value to the average value -- done
-   */
   for (int i = 0; i < height; i++) {
     for (int j = 0; j < width; j++) {
       RGBTRIPLE pixel = image[i][j];
@@ -22,50 +25,8 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width]) {
   return;
 }
 
-int sepia_helper(int num) {
-  if (num > 255) {
-    num = 255;
-  }
-  return num;
-}
-
-// Convert image to sepia
-void sepia(int height, int width, RGBTRIPLE image[height][width]) {
-  for (int i = 0; i < height; i++) {
-    for (int j = 0; j < width; j++) {
-      RGBTRIPLE pixel = image[i][j];
-      int sepiaRed =
-          sepia_helper(round(0.393 * pixel.rgbtRed + 0.769 * pixel.rgbtGreen +
-                             0.189 * pixel.rgbtBlue));
-      int sepiaGreen =
-          sepia_helper(round(0.349 * pixel.rgbtRed + 0.686 * pixel.rgbtGreen +
-                             0.168 * pixel.rgbtBlue));
-      int sepiaBlue =
-          sepia_helper(round(0.272 * pixel.rgbtRed + 0.534 * pixel.rgbtGreen +
-                             0.131 * pixel.rgbtBlue));
-
-      image[i][j].rgbtRed = sepiaRed;
-      image[i][j].rgbtGreen = sepiaGreen;
-      image[i][j].rgbtBlue = sepiaBlue;
-
-      // not working code -- buggy
-      /*if (sizeof(image[i][j].rgbtRed) > sizeof(BYTE)) {*/
-      /*  image[i][j].rgbtRed = 255;*/
-      /*}*/
-      /*if (sizeof(image[i][j].rgbtGreen) > sizeof(BYTE)) {*/
-      /*  image[i][j].rgbtGreen = 255;*/
-      /*}*/
-      /*if (sizeof(image[i][j].rgbtBlue) > sizeof(BYTE)) {*/
-      /*  image[i][j].rgbtBlue = 255;*/
-      /*}*/
-    }
-  }
-  return;
-}
-
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width]) {
-  // placeholder
   RGBTRIPLE buffer;
   for (int i = 0; i < height; i++) {
     for (int j = 0, last = width; j < width / 2; j++) {
@@ -74,19 +35,12 @@ void reflect(int height, int width, RGBTRIPLE image[height][width]) {
       image[i][last - j - 1] = buffer;
     }
   }
+
   return;
 }
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width]) {
-  /*TODO: function buggy: -- FIXED
-   * the bug is that when the image is copied it has not fully copied, so the
-   * pixels that are not copied show as zeros.
-   * NEED FIXED: change all img2 to image[i][j] -- DONE
-   * function buggy: -- FIXED
-   * need to count the pixels that are between pixels
-   */
-
   // Create a copy of image
   RGBTRIPLE copy[height][width];
   for (int i = 0; i < height; i++) {
@@ -180,6 +134,97 @@ void blur(int height, int width, RGBTRIPLE image[height][width]) {
       image[i][j].rgbtRed = round(average_red / counter);
       /*printf("%i, %i, %i\n", image[i][j].rgbtRed, image[i][j].rgbtGreen,*/
       /*       image[i][j].rgbtBlue);*/
+    }
+  }
+  return;
+}
+
+int average_G(int height, int width, RGBTRIPLE image[height][width], int i,
+              int j) {
+  int average_Gx_red =
+      image[i + 1][j - 1].rgbtRed * -1 + image[i][j - 1].rgbtRed * -2 +
+      image[i - 1][j - 1].rgbtRed * -1 + image[i + 1][j + 1].rgbtRed * 1 +
+      image[i - 1][j + 1].rgbtRed * 1 + image[i][j + 1].rgbtRed * 2;
+  int average_Gx_green =
+      image[i + 1][j - 1].rgbtGreen * -1 + image[i][j - 1].rgbtGreen * -2 +
+      image[i - 1][j - 1].rgbtGreen * -1 + image[i + 1][j + 1].rgbtGreen * 1 +
+      image[i - 1][j + 1].rgbtGreen * 1 + image[i][j + 1].rgbtGreen * 2;
+  int average_Gx_blue =
+      image[i + 1][j - 1].rgbtBlue * -1 + image[i][j - 1].rgbtBlue * -2 +
+      image[i - 1][j - 1].rgbtBlue * -1 + image[i + 1][j + 1].rgbtBlue * 1 +
+      image[i - 1][j + 1].rgbtBlue * 1 + image[i][j + 1].rgbtBlue * 2;
+
+  int average_Gy_red =
+      image[i - 1][j - 1].rgbtRed * -1 + image[i + 1][j - 1].rgbtRed * 1 +
+      image[i - 1][j].rgbtRed * -2 + image[i + 1][j].rgbtRed * 2 +
+      image[i - 1][j + 1].rgbtRed * -1 + image[i + 1][j + 1].rgbtRed * 1;
+  int average_Gy_green =
+      image[i - 1][j - 1].rgbtGreen * -1 + image[i + 1][j - 1].rgbtGreen * 1 +
+      image[i - 1][j].rgbtGreen * -2 + image[i + 1][j].rgbtGreen * 2 +
+      image[i - 1][j + 1].rgbtGreen * -1 + image[i + 1][j + 1].rgbtGreen * 1;
+  int average_Gy_blue =
+      image[i - 1][j - 1].rgbtBlue * -1 + image[i + 1][j - 1].rgbtBlue * 1 +
+      image[i - 1][j].rgbtBlue * -2 + image[i + 1][j].rgbtBlue * 2 +
+      image[i - 1][j + 1].rgbtBlue * -1 + image[i + 1][j + 1].rgbtBlue * 1;
+
+  printf("%i\n", average_Gx_red);
+  printf("%i\n", average_Gx_green);
+  printf("%i\n", average_Gx_blue);
+  printf("%i\n", average_Gy_red);
+  printf("%i\n", average_Gy_green);
+  printf("%i\n", average_Gy_blue);
+  return 0;
+}
+
+// Detect edges
+void edges(int height, int width, RGBTRIPLE image[height][width]) {
+  // Placeholder
+  height = 3;
+  width = 3;
+  // Image with varying colors
+  RGBTRIPLE img2[3][3];
+  img2[0][0] = pixel(0, 0, 255);
+  img2[0][1] = pixel(128, 0, 128);
+  img2[0][2] = pixel(255, 0, 0);
+
+  img2[1][0] = pixel(0, 0, 255);
+  img2[1][1] = pixel(128, 0, 128);
+  img2[1][2] = pixel(255, 0, 0);
+
+  img2[2][0] = pixel(0, 0, 255);
+  img2[2][1] = pixel(128, 0, 128);
+  img2[2][2] = pixel(255, 0, 0);
+
+  /*RGBTRIPLE img5[4][4];*/
+  /*img5[0][0] = pixel(10, 10, 10);*/
+  /*img5[0][1] = pixel(10, 10, 10);*/
+  /*img5[0][2] = pixel(10, 10, 10);*/
+  /*img5[0][3] = pixel(10, 10, 10);*/
+  /*img5[1][0] = pixel(10, 10, 10);*/
+  /*img5[1][1] = pixel(10, 10, 10);*/
+  /*img5[1][2] = pixel(10, 10, 10);*/
+  /*img5[1][3] = pixel(10, 10, 10);*/
+  /*img5[2][0] = pixel(10, 10, 10);*/
+  /*img5[2][1] = pixel(10, 10, 10);*/
+  /*img5[2][2] = pixel(10, 10, 10);*/
+  /*img5[2][3] = pixel(10, 10, 10);*/
+  /*img5[3][0] = pixel(10, 10, 10);*/
+  /*img5[3][1] = pixel(10, 10, 10);*/
+  /*img5[3][2] = pixel(10, 10, 10);*/
+  /*img5[3][3] = pixel(10, 10, 10);*/
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      image[i][j] = img2[i][j];
+    }
+  }
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
+      // if the pixel has 8 neighbors
+      int Gx;
+      int Gy;
+      if (j - 1 >= 0 && j + 1 < width && i - 1 >= 0 && i + 1 < height) {
+        average_G(height, width, img2, i, j);
+      }
     }
   }
   return;
